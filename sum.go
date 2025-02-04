17ffdbcd258c5e3f7679822bddfd2f7c0e1dd45f
renatohvo <renatohvo@gmail.com>
2025-02-03 22:03:03 -0300

    feat: sum.go quality gate

--- sum.go
+++ sum.go
@@ -9,12 +9,12 @@ func main()  {
 func sum(a int, b int) int {
 	return a + b
 }
-func sub(a int, b int) int {
-	return a - b
-}
-func times(a int, b int) int {
-	return a * b
-}
-func sumX(a int, b int) int {
-	return a + b + a
-}
+// func sub(a int, b int) int {
+// 	return a - b
+// }
+// func times(a int, b int) int {
+// 	return a * b
+// }
+// func sumX(a int, b int) int {
+// 	return a + b + a
+// }
