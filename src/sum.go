89c6b0bfa61b2fc1132fa1190da70f7749be81c2
renatohvo <renatohvo@gmail.com>
2025-02-04 20:11:53 -0300

    ci: sonar coverage

--- src/sum.go
+++ src/sum.go
@@ -1,14 +1,13 @@
 package main
 
-import "fmt"
-
-func main()  {
-	fmt.Println(sum(2, 2))
-}
+// func main()  {
+// 	fmt.Println(sum(2, 2))
+// }
 
 func sum(a int, b int) int {
 	return a + b
 }
+
 // func sub(a int, b int) int {
 // 	return a - b
 // }
